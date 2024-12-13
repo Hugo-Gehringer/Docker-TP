623e21c3026139a3e91fd867d6ce66e9ebaeecca
Maxime PALLEJA <maxime.palleja@gmail.com>
2024-12-13 12:31:31 +0100

    verifiaction de la connexion à la bdd

--- app.py
+++ app.py
@@ -5,23 +5,37 @@ import os
 app = Flask(__name__)
 
 def get_db_connection():
-    conn = psycopg2.connect(
-        host="db",
-        database="flaskdb",
-        user="user",
-        password="password"
-    )
-    return conn
+    try:
+        conn = psycopg2.connect(
+            host="db",
+            database="flaskdb",
+            user="user",
+            password="password"
+        )
+        return conn
+    except psycopg2.Error as e:
+        print("Erreur de connexion à la base de données", e)
+        return None
 
-@app.route("/")
+@app.route("/home")
 def home():
+    return "Bienvenue sur Flask !"
+
+@app.route("/check-db")
+def check_db():
     conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute("SELECT 'Bienvenue sur Flask avec PostgreSQL!'")
-    message = cur.fetchone()[0]
-    cur.close()
-    conn.close()
-    return message
+    if conn is None:
+        return "Échec de la connexion à la base de données", 500
+
+    try:
+        cur = conn.cursor()
+        cur.execute("SELECT 'Connexion à la base de données réussie!'")
+        message = cur.fetchone()[0]
+        cur.close()
+        conn.close()
+        return message
+    except psycopg2.Error as e:
+        return f"Erreur lors de l'exécution de la requête: {e}", 500
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
