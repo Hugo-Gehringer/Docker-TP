31a599e1f1dc67129d98d64fc8e3192266975c5c
gehringerhugo <hugo.gehringer@free.fr>
2024-12-13 12:11:53 +0100

    app db connect

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from flask import Flask
+import psycopg2
+import os
 
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = psycopg2.connect(
+        host="db",
+        database="flaskdb",
+        user="user",
+        password="password"
+    )
+    return conn
+
 @app.route("/")
 def home():
-    return "Bienvenue sur Flask !"
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT 'Bienvenue sur Flask avec PostgreSQL!'")
+    message = cur.fetchone()[0]
+    cur.close()
+    conn.close()
+    return message
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
