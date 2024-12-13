8b11229a4218b23a4783f19ee295b02a898dda1e
Maxime PALLEJA <maxime.palleja@gmail.com>
2024-12-13 13:20:54 +0100

    env et secret dans app.py

--- app.py
+++ app.py
@@ -6,10 +6,22 @@ app = Flask(__name__)
 
 def get_db_connection():
     try:
-        db_user = os.getenv('DB_USER')
-        db_password = os.getenv('DB_PASSWORD')
-        db_name = os.getenv('DB_NAME')
-        db_host = os.getenv('DB_HOST')
+        db_user = None
+        db_password = None
+        db_name = None
+        db_host = None
+
+        if os.path.exists('/run/secrets/db_user'):
+            db_user = open('/run/secrets/db_user').read().strip()
+            db_password = open('/run/secrets/db_password').read().strip()
+            db_name = open('/run/secrets/db_name').read().strip()
+            db_host = open('/run/secrets/db_host').read().strip()
+
+        elif os.getenv('DB_USER') is not None:
+            db_user = os.getenv('DB_USER')
+            db_password = os.getenv('DB_PASSWORD')
+            db_name = os.getenv('DB_NAME')
+            db_host = os.getenv('DB_HOST')
 
         conn = psycopg2.connect(
             host=db_host,
